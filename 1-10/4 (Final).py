4c0439d2ed719f8cdd27b2d0e31c219822fffe21
johnpmarshall2000 <johnpmarshall2000@gmail.com>
2023-11-13 09:15:54 -0500

    Minor Change to Algorithm and Timing

--- 1-10/4 (Final).py
+++ 1-10/4 (Final).py
@@ -1,19 +1,27 @@
-import time
+from timeit import default_timer as timer
 
-start = time.time()
+start = timer()
 iterations = 0
-# Check only to the root of the value
+
+# Iterate over the possible palindromes
 found = False
 for i in range(999, 99, -1):
     value = int(str(i) + str(i)[::-1])
 
+    # Check only to the root of the value
     for j in range(999, int(value ** 0.5), -1):
         iterations += 1
-        other_factor = value // j
-        if value % j == 0 and other_factor <= 999 and other_factor >= 100:
-            print(value)
-            found = True
+
+        # Check divisibility
+        if value % j == 0:
+
+            # Check if the other factor is 3 digits
+            other_factor = value // j
+            if other_factor <= 999 and other_factor >= 100:
+                print(value)
+                found = True
+                break
     
     if found:break
 
-print(f"Elapsed Time: {time.time() - start}")
+print(f"Elapsed Time: {timer() - start}")
